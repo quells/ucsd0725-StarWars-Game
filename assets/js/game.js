f1d07a5660fd379e603106ff716b0090632e6f56
Kai Wells <support@kaiwells.me>
2017-08-24 15:10:18 -0700

    Game skeleton

--- assets/js/game.js
+++ assets/js/game.js
@@ -33,3 +33,56 @@ var CharacterView = function (c) {
     $("<div>").addClass("clear").appendTo(root);
     return root;
 }
+
+var Game = function(characters, updateUI) {
+    this.updateUI = updateUI;
+
+    this.characters = characters;
+    this.player = undefined;
+    this.remainingOpponents = [];
+    this.defeatedOpponents = [];
+
+    this.state = "NEW_GAME";
+    this.states = ["NEW_GAME", "CHOOSE_PLAYER", "CHOOSE_OPPONENT", "FIGHT_SCREEN", "ATTACKING", "GAME_LOST", "GAME_WON"];
+    this.transitions = {
+        "NEW_GAME": ["CHOOSE_PLAYER"],
+        "CHOOSE_PLAYER": ["FIGHT_SCREEN"],
+        "CHOOSE_OPPONENT": ["FIGHT_SCREEN", "GAME_WON"],
+        "FIGHT_SCREEN": ["ATTACKING", "CHOOSE_OPPONENT"],
+        "ATTACKING": ["FIGHT_SCREEN", "CHOOSE_OPPONENT", "GAME_LOST"],
+        "GAME_LOST": ["NEW_GAME"],
+        "GAME_WON": ["NEW_GAME"]
+    };
+
+    this.canTransitionTo = function(nextState) {
+        var validTransitions = this.transitions[this.state];
+        for (var i = 0; i < validTransitions.length; i++) {
+            if (validTransitions[i] == nextState) { return true; }
+        }
+        return false;
+    };
+
+    this.transitionTo = function(nextState) {
+        if (this.canTransitionTo(nextState)) {
+            this.state = nextState;
+            this.handleCurrentState();
+            return;
+        } else {
+            throw "Cannot transition from " + this.state + " to " + nextState;
+        }
+    };
+
+    this.handleCurrentState = function() {
+        switch (this.state) {
+            case "NEW_GAME":
+            case "CHOOSE_PLAYER":
+            case "CHOOSE_OPPONENT":
+            case "FIGHT_SCREEN":
+            case "ATTACKING":
+            case "GAME_LOST":
+            case "GAME_WON":
+            default:
+                throw "Have not implemented " + this.state;
+        }
+    };
+};
