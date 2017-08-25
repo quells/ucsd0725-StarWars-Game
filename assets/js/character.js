23a9afe95b5bd859c960334919e5b0ee4ac8b5d7
Kai Wells <support@kaiwells.me>
2017-08-25 13:49:17 -0700

    Pull out Character from Game

---
+++ assets/js/character.js
@@ -0,0 +1,41 @@
+var Character = function(shortName, name, weapon, power, health, color) {
+    this.shortName = shortName;
+    this.name = name;
+    this.weapon = weapon;
+    this.brandColor = color;
+
+    this.basePower = power;
+    this.attackPower = power;
+    this.defendPower = power;
+
+    this.maxHealth = health;
+    this.health = health;
+
+    this.attack = function(other) {
+        other.health -= this.attackPower;
+        if (other.health > 0) {
+            this.health -= other.defendPower;
+        }
+        this.attackPower += this.basePower;
+        return other;
+    }
+
+    this.reset = function() {
+        this.attackPower = this.basePower;
+        this.health = this.maxHealth;
+        return this;
+    }
+}
+
+var CharacterView = function (c) {
+    var root = $("<div>").addClass("character " + c.shortName).attr("shortname", c.shortName);
+    var avatar = $("<div>").addClass("charAvatar").appendTo(root);
+    $("<img>").addClass("avatarImage").attr("src", "assets/img/" + c.shortName + ".png").appendTo(avatar);
+	$("<canvas>").addClass("avatarHealth").attr("name", c.shortName).attr("brandcolor", c.brandColor).appendTo(avatar);
+    var description = $("<div>").addClass("charDescription").appendTo(root);
+    $("<h3>").addClass("charName").text(c.name).appendTo(description);
+    $("<p>").addClass("charWeapon").text("Weapon: " + c.weapon).appendTo(description);
+    $("<p>").addClass("charStatus").text("POW: " + c.attackPower + " HP: " + c.health).appendTo(description);
+    $("<div>").addClass("clear").appendTo(root);
+    return root;
+}
--- assets/js/game.js
+++ assets/js/game.js
@@ -1,52 +1,10 @@
-var Character = function(shortName, name, weapon, power, health, color) {
-    this.shortName = shortName;
-    this.name = name;
-    this.weapon = weapon;
-    this.brandColor = color;
-
-    this.basePower = power;
-    this.attackPower = power;
-    this.defendPower = power;
-
-    this.maxHealth = health;
-    this.health = health;
-
-    this.attack = function(other) {
-        other.health -= this.attackPower;
-        if (other.health > 0) {
-            this.health -= other.defendPower;
-        }
-        this.attackPower += this.basePower;
-        return other;
-    }
-
-    this.reset = function() {
-        this.attackPower = this.basePower;
-        this.health = this.maxHealth;
-        return this;
-    }
-}
-
-var CharacterView = function (c) {
-    var root = $("<div>").addClass("character " + c.shortName);
-    var avatar = $("<div>").addClass("charAvatar").appendTo(root);
-    $("<img>").addClass("avatarImage").attr("src", "assets/img/" + c.shortName + ".png").appendTo(avatar);
-	$("<canvas>").addClass("avatarHealth").attr("name", c.shortName).attr("brandcolor", c.brandColor).appendTo(avatar);
-    var description = $("<div>").addClass("charDescription").appendTo(root);
-    $("<h3>").addClass("charName").text(c.name).appendTo(description);
-    $("<p>").addClass("charWeapon").text("Weapon: " + c.weapon).appendTo(description);
-    $("<p>").addClass("charStatus").text("POW: " + c.attackPower + " HP: " + c.health).appendTo(description);
-    $("<div>").addClass("clear").appendTo(root);
-    return root;
-}
-
 var Game = function(characters, updateUI) {
     this.updateUI = updateUI;
 
     this.characters = characters;
     this.player = undefined;
-    this.remainingOpponents = [];
-    this.defeatedOpponents = [];
+    this.remainingOpponents = {};
+    this.defeatedOpponents = {};
 
     this.state = "NEW_GAME";
     this.states = ["NEW_GAME", "CHOOSE_PLAYER", "CHOOSE_OPPONENT", "FIGHT_SCREEN", "ATTACKING", "GAME_LOST", "GAME_WON"];
@@ -95,5 +53,23 @@ var Game = function(characters, updateUI) {
                 throw "Have not implemented " + this.state;
         }
         this.updateUI();
+    };
+
+    this.selectCharacter = function(event) {
+        var charName = $(event.currentTarget).attr("shortname")
+        var isPlayer = ((this.player != undefined) && (charName == this.player.shortName));
+        switch (this.state) {
+            case "NEW_GAME":
+                console.log(charName);
+                break;
+            case "CHOOSE_PLAYER":
+            case "CHOOSE_OPPONENT":
+            case "FIGHT_SCREEN":
+            case "ATTACKING":
+            case "GAME_LOST":
+            case "GAME_WON":
+            default:
+                throw "Have not implemented " + this.state;
+        }
     };
 };
